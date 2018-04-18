017f663b8084753f36c6f71d58c4aabdafda990a
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-04-18 12:00:18 -0700

    feat(content-carousel): upgrade content carousel to a web component
    polyfills included into demo app.

--- @uportal/content-carousel/src/main.js
+++ @uportal/content-carousel/src/main.js
@@ -1,8 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import ContentCarousel from "./content-carousel.vue";
+import wrap from "@vue/web-component-wrapper";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+window.customElements.define("content-carousel", wrap(Vue, ContentCarousel));
