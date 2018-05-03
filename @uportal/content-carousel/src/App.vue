3a38354445b01b60d8bdeed66b5c1b562a015387
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-05-03 11:11:16 -0700

    feat: setup test, export web component, render images

--- @uportal/content-carousel/src/App.vue
+++ @uportal/content-carousel/src/App.vue
@@ -5,26 +5,10 @@
     :navigationEnabled="true"
     :paginationEnabled="false"
   >
-    <slide>
-      Slide 1 Content
-    </slide>
-    <slide>
-      Slide 2 Content
-    </slide>
-    <slide>
-      Slide 2 Content
-    </slide>
-    <slide>
-      Slide 2 Content
-    </slide>
-    <slide>
-      Slide 2 Content
-    </slide>
-    <slide>
-      Slide 2 Content
-    </slide>
-    <slide>
-      Slide 2 Content
+    <slide v-for="item in items" :key="item.id">
+      <a v-bind:href="item.destinationHref">
+        <img width="100%" v-bind:src="item.imageSrc" v-bind:alt="item.imageDescription">
+      </a>
     </slide>
   </carousel>
 </template>
@@ -36,6 +20,47 @@ export default {
   components: {
     Carousel,
     Slide
+  },
+  data() {
+    return {
+      items: [
+        {
+          id: "unique1",
+          destinationHref: "https://example.com",
+          imageSrc:
+            "https://images.unsplash.com/photo-1519104482037-8b4a7c13ad29",
+          imageDescription: "example"
+        },
+        {
+          id: "unique2",
+          destinationHref: "https://example.com",
+          imageSrc:
+            "https://images.unsplash.com/photo-1508485654983-d3ff70e70357",
+          imageDescription: "example"
+        },
+        {
+          id: "unique3",
+          destinationHref: "https://example.com",
+          imageSrc:
+            "https://images.unsplash.com/photo-1504786328163-b16f428adc28",
+          imageDescription: "example"
+        },
+        {
+          id: "unique4",
+          destinationHref: "https://example.com",
+          imageSrc:
+            "https://images.unsplash.com/photo-1522205940279-d75807ebcd91",
+          imageDescription: "example"
+        },
+        {
+          id: "unique5",
+          destinationHref: "https://example.com",
+          imageSrc:
+            "https://images.unsplash.com/photo-1504466664756-1adbe6d13b36",
+          imageDescription: "example"
+        }
+      ]
+    };
   }
 };
 </script>
