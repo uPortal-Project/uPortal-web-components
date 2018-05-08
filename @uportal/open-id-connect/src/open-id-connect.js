188db5815973be6cab1cd8990c2c799d23374cc0
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-05-08 07:42:25 -0700

    feat(open-id-connect): create shared oidc helper for uPortal

---
+++ @uportal/open-id-connect/src/open-id-connect.js
@@ -0,0 +1,29 @@
+let bearerToken = null;
+
+export default async function openIdConnec({
+  setTimeout = setTimeout, // allow a custom timeout function to be used
+  userInfoApiUrl = '/uPortal/api/v5-1/userinfo',
+  tokenTimeoutMs = 180000, // 3 minutes
+  callback = () => {}, // optional callback
+}) {
+  if (bearerToken !== null) {
+    return bearerToken;
+  }
+
+  const response = await fetch(userInfoApiUrl, {
+    credentials: 'same-origin',
+  });
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  bearerToken = await response.text();
+
+  // clear token after timeout has passed
+  setTimeout(() => {
+    bearerToken = null;
+  }, tokenTimeoutMs);
+
+  callback(bearerToken);
+
+  return bearerToken;
+}
