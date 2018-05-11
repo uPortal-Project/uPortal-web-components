749fe1ce0d6f9cc887b43bc6d720226d2d5b2b20
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-05-11 12:27:48 -0700

    feat(content-carousel): add better styles for the content objects

--- @uportal/content-carousel/src/App.vue
+++ @uportal/content-carousel/src/App.vue
@@ -1,17 +1,59 @@
 <template>
-  <carousel
-    :perPage="1"
-    :perPageCustom="[[480, 2], [768, 3]]"
-    :navigationEnabled="true"
-    :paginationEnabled="false"
-  >
-    <slide v-for="item in items" :key="item.id">
-      <a v-bind:href="item.destinationHref">
-        <img width="100%" v-bind:src="item.imageSrc" v-bind:alt="item.imageDescription">
-      </a>
-    </slide>
-  </carousel>
+  <section>
+    <h1>Recommended</h1>
+    <carousel
+      :perPage="1"
+      :perPageCustom="[[1, 2], [480, 4], [768, 6]]"
+      :navigationEnabled="true"
+      :paginationEnabled="false"
+    >
+      <slide v-for="item in items" :key="item.id">
+        <a v-bind:href="item.destinationHref">
+          <img width="100%" v-bind:src="item.imageSrc" v-bind:alt="item.imageDescription">
+          <section class="item-title">
+            {{ item.imageDescription }}
+          </section>
+        </a>
+      </slide>
+    </carousel>
+  </section>
 </template>
+
+<style>
+html,
+body {
+  width: 100%;
+  margin: 0;
+  overflow: hidden;
+}
+
+body {
+  font-family: sans-serif;
+  padding: 10px;
+}
+
+h1 {
+  margin-bottom: 0;
+  margin-left: 10px;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+.item-title {
+  padding: 2px 5px 5px 5px;
+}
+
+.VueCarousel-slide {
+  /* border: 1px solid grey; */
+  box-shadow: 0 2px 4px grey;
+  border-radius: 2px;
+  overflow: hidden;
+  margin: 10px;
+}
+</style>
 
 <script>
 import { Carousel, Slide } from "vue-carousel";
@@ -29,35 +71,56 @@ export default {
           destinationHref: "https://example.com",
           imageSrc:
             "https://images.unsplash.com/photo-1519104482037-8b4a7c13ad29",
-          imageDescription: "example"
+          imageDescription: "Big Sporting Event"
         },
         {
           id: "unique2",
           destinationHref: "https://example.com",
           imageSrc:
             "https://images.unsplash.com/photo-1508485654983-d3ff70e70357",
-          imageDescription: "example"
+          imageDescription: "Campus Map"
         },
         {
           id: "unique3",
           destinationHref: "https://example.com",
           imageSrc:
             "https://images.unsplash.com/photo-1504786328163-b16f428adc28",
-          imageDescription: "example"
+          imageDescription: "Parking Permit Renewal"
         },
         {
           id: "unique4",
           destinationHref: "https://example.com",
           imageSrc:
             "https://images.unsplash.com/photo-1522205940279-d75807ebcd91",
-          imageDescription: "example"
+          imageDescription: "Computer Lab Update Hours"
         },
         {
           id: "unique5",
           destinationHref: "https://example.com",
           imageSrc:
             "https://images.unsplash.com/photo-1504466664756-1adbe6d13b36",
-          imageDescription: "example"
+          imageDescription: "Photo of the week by John Doe"
+        },
+        {
+          id: "unique6",
+          destinationHref: "https://example.com",
+          imageSrc:
+            "https://images.unsplash.com/photo-1508693926297-1d61ee3df82a",
+          imageDescription: "Fall Colors Trip"
+        },
+        {
+          id: "unique7",
+          destinationHref: "https://example.com",
+          imageSrc:
+            "https://images.unsplash.com/photo-1515614457209-3c7081d62743",
+          imageDescription: "Special Guest Talk: Energy Conservation"
+        },
+        {
+          id: "unique7",
+          destinationHref: "https://example.com",
+          imageSrc:
+            "https://images.unsplash.com/photo-1490984902283-b7f4723f3955",
+          imageDescription: "Please stop feeding the gulls"
         }
       ]
     };
