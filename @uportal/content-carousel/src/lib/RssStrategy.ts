e7641ecc863e64372e4707519ab7ab9ed9d96e66
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-06-18 14:56:54 -0700

    refactor: use array map to generate new array automatically

--- @uportal/content-carousel/src/lib/RssStrategy.ts
+++ @uportal/content-carousel/src/lib/RssStrategy.ts
@@ -3,36 +3,44 @@ import { CarouselItem } from '@/lib/CarouselItem';
 import { parseXml } from '@/lib/parse';
 
 export class RssStrategy implements DataStrategy {
-    public readonly type = 'RSS';
+  public readonly type = 'RSS';
 
-    public readonly items: CarouselItem[] = [];
+  public readonly items: CarouselItem[] = [];
 
-    constructor(
-        public feed: string,
-    ) {
-        this.load(feed);
+  constructor(public feed: string) {
+    this.load(feed);
+  }
+
+  private async load(path: string): Promise<any> {
+    const response = await fetch(`${path}`);
+    if (!response.ok) {
+      throw new Error(response.statusText);
     }
+    const feed = await response.text();
 
-    private async load(path: string): Promise<any> {
-        const response = await fetch(`${path}`);
-        if (!response.ok) {
-            throw new Error(response.statusText);
-        }
-        const feed = await response.text();
-
-        return parseXml(feed).then(
-            (data) => {
-                data.items.forEach((item: any, index: number) => {
-                    const { title, description, link } = item;
-                    this.items.push({
-                        id: `${index}-${new Date().getTime()}`,
-                        altText: `${title} - ${description}`,
-                        destinationUrl: link,
-                        imageUrl: item.media ? item.media.content : item.enclosures ? item.enclosures[0] : null,
-                    });
-                });
-                return this.items;
-            },
-        );
-    }
+    const { items } = await parseXml(feed);
+    this.items = items.map(
+      (
+        {
+          title,
+          description,
+          link,
+          media,
+          enclosures,
+        }: {
+          title: string;
+          description: string;
+          link: string;
+          media: any;
+          enclosures: any;
+        },
+        index: number
+      ) => ({
+        id: `${index}-${new Date().getTime()}`,
+        altText: `${title} - ${description}`,
+        destinationUrl: link,
+        imageUrl: media ? media.content : enclosures ? enclosures[0] : null,
+      })
+    );
+  }
 }
