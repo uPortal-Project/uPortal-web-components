caf8d7cf755499f74be9c897cffb103b311ff1fa
GIP RECIA - Julien Gribonvald <julien.gribonvald@gmail.com>
2018-06-22 18:42:20 +0200

    feat(portlet-card): adding portlet-card web-component

---
+++ @uportal/portlet-card/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <portlet-card fname="test" title="Un titre de test" description="Une description plutôt longue pour faire fonctionner le clamp.js et voir ce qu'il se passe"
+                  :is-favorite="true" can-favorites="true" icon-background-color="#0088bf" icon-url="favicon.ico"></portlet-card>
+  </div>
+</template>
+
+<script>
+import PortletCard from './components/PortletCard.vue'
+
+export default {
+  name: 'app',
+  components: {
+    PortletCard
+  }
+}
+</script>
+
+<style>
+  #app{
+    margin:50px;
+  }
+</style>
+
