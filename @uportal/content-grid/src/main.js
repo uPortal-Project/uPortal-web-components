f9df60b2d2326d57c31fcda4dca8955926f0e734
GIP RECIA - Julien Gribonvald <julien.gribonvald@gmail.com>
2018-06-27 15:23:23 +0200

    build(content-grid): fix content and new test build

--- @uportal/content-grid/src/main.js
+++ @uportal/content-grid/src/main.js
@@ -1,17 +1,9 @@
 import Vue from "vue";
-import lineClamp from "vue-line-clamp";
 import ContentGrid from "./App.vue";
-import VueI18n from "vue-i18n";
-import i18n from "./i18n.js";
-
-Vue.use(lineClamp, {
-  // plugin options
-});
 
 Vue.config.productionTip = false;
 
 new Vue({
   el: "#app",
-  i18n,
   render: h => h(ContentGrid)
 });
