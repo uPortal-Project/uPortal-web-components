ae0431fff10922c1d21b5c96e347c89b70f98e0c
Phillip Ball <phillip@phillipball.com>
2018-08-21 08:54:41 -0700

    fix: data processing - boils out all duplicates within portlet array.

--- @uportal/portlet-registry-to-array/src/portlet-registry-to-array.js
+++ @uportal/portlet-registry-to-array/src/portlet-registry-to-array.js
@@ -1,3 +1,5 @@
+import uniqBy from 'lodash/uniqBy';
+
 /**
  * Combines a array of arrays into a single level array
  * @param {Array<Portlet>} acc - accululator that combines all the arrays
@@ -9,13 +11,23 @@ function flatten(acc, arr) {
 }
 
 /**
- * Walks the portlet registry tree
+ * Takes the returned array from treeWalker and removes duplicates
+ * baed on "fname"
  * @param {Object} registryJson Portlet Registry Tree
  * @return {Array<Portlet>} list of portlets
  */
 export function portletRegistryToArray(registryJson) {
+  return uniqBy(treeWalker(registryJson), 'fname');
+}
+
+/**
+ * Walks the portlet registry tree
+ * @param {Object} registryJson Portlet Registry Tree
+ * @return {Array<Portlet>} list of portlets
+ */
+function treeWalker(registryJson) {
   if (registryJson.registry) {
-    return portletRegistryToArray(registryJson.registry);
+    return treeWalker(registryJson.registry);
   }
 
   const portlets = registryJson.portlets || [];
