3784c61fba848335949077f3d632ba90427541cd
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-10-29 11:32:21 -0700

    fix: manually tree shake font awesome icons
    webpack was not correctly identinifying unused icons, leading to 700kb
    of unused icons being included in package.

--- @uportal/esco-content-menu/src/icons.js
+++ @uportal/esco-content-menu/src/icons.js
@@ -1,14 +1,12 @@
 import {library} from '@fortawesome/fontawesome-svg-core';
-import {faStar as faStarRegular} from '@fortawesome/free-regular-svg-icons';
-import {
-  faChevronLeft,
-  faChevronRight,
-  faExchangeAlt,
-  faSignOutAlt,
-  faStar as faStarSolid,
-  faTimes,
-  faUser,
-} from '@fortawesome/free-solid-svg-icons';
+import {faStar as faStarRegular} from '@fortawesome/free-regular-svg-icons/faStar';
+import {faChevronLeft} from '@fortawesome/free-solid-svg-icons/faChevronLeft';
+import {faChevronRight} from '@fortawesome/free-solid-svg-icons/faChevronRight';
+import {faExchangeAlt} from '@fortawesome/free-solid-svg-icons/faExchangeAlt';
+import {faSignOutAlt} from '@fortawesome/free-solid-svg-icons/faSignOutAlt';
+import {faStar as faStarSolid} from '@fortawesome/free-solid-svg-icons/faStar';
+import {faTimes} from '@fortawesome/free-solid-svg-icons/faTimes';
+import {faUser} from '@fortawesome/free-solid-svg-icons/faUser';
 
 if (process.env.NODE_ENV === 'development') {
   console.log(
