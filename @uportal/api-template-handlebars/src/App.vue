9873f0bc0c0e07c61baf4b32ee6c58d55a34596d
Drew Wills <wills.drew@gmail.com>
2019-01-04 14:05:33 -0700

    feat: rename HelloWorld -> ApiTemplateHandlebars; install axios

--- @uportal/api-template-handlebars/src/App.vue
+++ @uportal/api-template-handlebars/src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <api-template-handlebars />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import ApiTemplateHandlebars from './components/ApiTemplateHandlebars.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ApiTemplateHandlebars
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
