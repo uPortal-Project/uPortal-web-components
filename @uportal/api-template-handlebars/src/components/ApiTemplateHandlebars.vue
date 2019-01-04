33e9f36926897fb14cafb0e0b97ab5c7abe5faa2
Drew Wills <wills.drew@gmail.com>
2019-01-04 14:05:33 -0700

    feat: rename HelloWorld -> ApiTemplateHandlebars; install axios

---
+++ @uportal/api-template-handlebars/src/components/ApiTemplateHandlebars.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Hello World!</h2>
+</template>
+
+<script>
+export default {
+  name: 'ApiTemplateHandlebars',
+  props: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
