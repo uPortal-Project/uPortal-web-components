6e7626088e0021f949ddf7df85eff8deae6b4291
Ryan Mathis <rmathis@unicon.net>
2019-06-14 08:52:20 -0700

    feat: moved content renderers into their own sub-repo

---
+++ @uportal/content-renderer/src/components/PortletRenderer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div v-html="portlet" />
+</template>
+
+<script>
+import ky from 'ky';
+import oidc from '@uportal/open-id-connect';
+import Vue from 'vue';
+import AsyncComputed from 'vue-async-computed';
+Vue.use(AsyncComputed);
+
+export default {
+  name: 'PortletRenderer',
+  props: {
+    portletHtmlUrl: String,
+    debug: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  asyncComputed: {
+    portlet: {
+      async get() {
+        const {portletHtmlUrl, debug} = this;
+        try {
+          const headers = debug
+            ? {}
+            : {
+              'Authorization': 'Bearer ' + (await oidc()).encoded,
+              'content-type': 'application/jwt',
+            };
+          return await ky.get(portletHtmlUrl, {headers}).text();
+        } catch (err) {
+          // eslint-disable-next-line no-console
+          console.error(err);
+          return '';
+        }
+      },
+    },
+  },
+};
+</script>
