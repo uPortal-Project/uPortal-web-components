71835131174d2ccb2f0f2b646cb09e4c439f6391
Curtis <curtis.wolfenberger@gmail.com>
2019-09-19 14:27:54 -0700

    feat(sidebar): layout and styles

--- @uportal/sidebar-nav/src/components/SidebarNav.vue
+++ @uportal/sidebar-nav/src/components/SidebarNav.vue
@@ -1,114 +1,111 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="sidebar-nav">
+    <div class="sidebar-nav__search"></div>
+
+    <div class="sidebar-nav__accordions">
+      <div class="accordion" v-for="n in 4" :key="n">
+        <a href="#" class="accordion__header">Accordion Header {{ n }}</a>
+        <ul>
+          <li v-for="link in 4" :key="link">
+            <a href="#">Link {{ link }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="sidebar-nav__info-box">
+      <h6>Need Help?</h6>
+      <a class="button" href="#">Grant Process Guide</a>
+      <a href="#">Contacts &raquo;</a>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'SidebarNav',
+  props: {}
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+<style lang="scss" scoped>
 a {
-  color: #42b983;
+  text-decoration: none;
+  font-family: 'Helvetica Neue', Helvetica, sans-serif;
+}
+
+.sidebar-nav {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  height: 100%;
+  width: 330px;
+  padding: 15px;
+  background-color: #435982;
+  overflow: auto;
+
+  &__accordions {
+    margin: 0 0 15px;
+
+    .accordion {
+      background-color: white;
+      border: 1px solid #dfdfdf;
+      border-radius: 5px;
+
+      &__header {
+        display: block;
+        background-color: #b2c6db;
+        padding: 25px 0 25px 35px;
+        color: #042158;
+      }
+
+      > ul {
+        display: block;
+        list-style: none;
+        margin: 0;
+        padding: 20px;
+
+        > li {
+          display: block;
+          color: #042158;
+          padding: 10px 15px;
+          border-bottom: 1px solid #bfbfbf;
+
+          > a {
+            color: #042158;
+          }
+        }
+      }
+    }
+  }
+
+  &__info-box {
+    background-color: #042158;
+    color: white;
+    padding: 20px 15px;
+    border-radius: 5px;
+
+    h6 {
+      margin: 0 0 15px;
+      font-size: 1rem;
+      font-weight: normal;
+      text-transform: uppercase;
+    }
+    .button {
+      display: block;
+      background-color: #194f9a;
+      color: white;
+      padding: 10px;
+      margin: 0 0 10px;
+      text-align: center;
+      border-radius: 5px;
+    }
+    a {
+      color: #aadbf6;
+      margin-left: 15px;
+      text-decoration: none;
+    }
+  }
 }
 </style>
