25c5e081aebe517b00785591fc1a1d9c92938e0c
gbrousse-recia <gregory.brousse@recia.fr>
2022-02-22 18:14:05 +0100

    feat(esco-content-menu): content-user component

--- @uportal/esco-content-menu-lit/src/locales/fr.ts
+++ @uportal/esco-content-menu-lit/src/locales/fr.ts
@@ -7,6 +7,14 @@ import { str } from '@lit/localize';
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export const templates = {
+  s222b63bb7b6cf89d: str`Changer d'établissement`,
+  s380927a4d5b227ed: str`Tous les services`,
+  s4b0eba4268a3c045: str`Rechercher un service...`,
+  s5379e362403c02bc: str`Vous n'avez pas de favoris`,
+  s79529ae47e56fbd5: str`Toutes les catégories`,
+  s8bdf1e8dfd70aa2c: str`Voir les informations de mon compte`,
+  s8f1dff5d47459b5e: str`Mes favoris`,
   s9e226e0a027f39a6: str`Supprimer des favoris`,
   scff3aac0935b2a74: str`Ajouter aux favoris`,
+  s1967faf9ed8d3504: str`Avatar`,
 };
