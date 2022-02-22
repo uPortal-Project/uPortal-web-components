dd1db1b3bc8c7acd5892ff354e4ace0aaf05f25a
gbrousse-recia <gregory.brousse@recia.fr>
2022-02-22 17:04:07 +0100

    feat(esco-content-menu): action-favorites component

---
+++ @uportal/esco-content-menu-lit/src/locales/fr.ts
@@ -0,0 +1,12 @@
+// Do not modify this file by hand!
+// Re-generate this file by running lit-localize
+
+import { str } from '@lit/localize';
+
+/* eslint-disable no-irregular-whitespace */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export const templates = {
+  s9e226e0a027f39a6: str`Supprimer des favoris`,
+  scff3aac0935b2a74: str`Ajouter aux favoris`,
+};
