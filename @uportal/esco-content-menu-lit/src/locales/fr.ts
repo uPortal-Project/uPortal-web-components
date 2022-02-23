c8aa69e26ee5d2dd3575c49b69c0b839c0c8ee82
gbrousse-recia <gregory.brousse@recia.fr>
2022-02-23 14:40:09 +0100

    fix(esco-content-menu): update locales

--- @uportal/esco-content-menu-lit/src/locales/fr.ts
+++ @uportal/esco-content-menu-lit/src/locales/fr.ts
@@ -7,6 +7,7 @@ import { str } from '@lit/localize';
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export const templates = {
+  s1d5d2564d7aae78e: str`Fermer le menu`,
   s222b63bb7b6cf89d: str`Changer d'établissement`,
   s380927a4d5b227ed: str`Tous les services`,
   s4b0eba4268a3c045: str`Rechercher un service...`,
@@ -14,9 +15,8 @@ export const templates = {
   s79529ae47e56fbd5: str`Toutes les catégories`,
   s8bdf1e8dfd70aa2c: str`Voir les informations de mon compte`,
   s8f1dff5d47459b5e: str`Mes favoris`,
+  s8fa032cdafb3c9b0: str`Se déconnecter`,
   s9e226e0a027f39a6: str`Supprimer des favoris`,
   scff3aac0935b2a74: str`Ajouter aux favoris`,
   s1967faf9ed8d3504: str`Avatar`,
-  s1d5d2564d7aae78e: str`Close menu`,
-  s8fa032cdafb3c9b0: str`Sign Out`,
 };
